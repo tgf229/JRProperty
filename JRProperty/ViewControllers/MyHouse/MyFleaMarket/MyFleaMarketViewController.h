8d56ddcdc7e4ea3ec08f83ad59a0d4edc2ac7b56
YMDQ <ymdq@ymdq-mbp15.local>
2015-12-29 20:09:37 +0800

    我的宝贝

---
+++ JRProperty/ViewControllers/MyHouse/MyFleaMarket/MyFleaMarketViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyFleaMarketViewController.h
+//  JRProperty
+//
+//  Created by YMDQ on 15/12/29.
+//  Copyright © 2015年 YRYZY. All rights reserved.
+//
+
+#import "JRViewController.h"
+
+@interface MyFleaMarketViewController : JRViewControllerWithBackButton
+
+@end
