2cb636c3174d9fcd52c8b0248828209a8279a466
YMDQ <ymdq@ymdq-mbp15.local>
2016-01-04 17:48:28 +0800

    我的消息2.0model

---
+++ JRProperty/Models/HomePage/MyMessageBoxListModel.h
@@ -0,0 +1,34 @@
+//
+//  MyMessageBoxListModel.h
+//  JRProperty
+//
+//  Created by YMDQ on 16/1/4.
+//  Copyright © 2016年 YRYZY. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@protocol MyMessageBoxModel
+
+@end
+
+@interface MyMessageBoxModel : JSONModel
+@property(copy,nonatomic) NSString<Optional> * mId;
+@property(copy,nonatomic) NSString<Optional> * type;
+@property(copy,nonatomic) NSString<Optional> * aId;
+@property(copy,nonatomic) NSString<Optional> * content;
+@property(copy,nonatomic) NSString<Optional> * time;
+@property(copy,nonatomic) NSString<Optional> * imageUrl;
+@property(copy,nonatomic) NSString<Optional> * replyUId;
+@property(copy,nonatomic) NSString<Optional> * replyNickName;
+@property(copy,nonatomic) NSString<Optional> * replyCommentId;
+@property(copy,nonatomic) NSString<Optional> * replyContent;
+@property(copy,nonatomic) NSString<Optional> * replyHeadUrl;
+@property(copy,nonatomic) NSString<Optional> * userLevel;
+@property(copy,nonatomic) NSString<Optional> * beReplyUId;
+@property(copy,nonatomic) NSString<Optional> * beReplyNickName;
+@end
+
+@interface MyMessageBoxListModel : BaseModel
+@property(nonatomic,strong) NSArray<MyMessageBoxModel,Optional> * doc;
+@end
---
+++ JRProperty/Models/HomePage/MyMessageBoxListModel.m
@@ -0,0 +1,13 @@
+//
+//  MyMessageBoxListModel.m
+//  JRProperty
+//
+//  Created by YMDQ on 16/1/4.
+//  Copyright © 2016年 YRYZY. All rights reserved.
+//
+
+#import "MyMessageBoxListModel.h"
+
+@implementation MyMessageBoxListModel
+
+@end
